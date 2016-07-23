f575406888feb2afcdfc9cce9438c3ba2ac3e48c
Harithamachanuru <harithamachanuru@gmail.com>
2016-07-23 14:22:53 +0530

    Add files via upload

---
+++ DependencyExample/src/com/capgemini/Employee.java
@@ -0,0 +1,35 @@
+package com.capgemini;
+
+public class Employee {
+	
+	private int employeeNumber;
+	private String name;
+	
+	private Address address;
+	private Address address2;
+
+	public int getEmployeeNumber() {
+		return employeeNumber;
+	}
+
+	public void setEmployeeNumber(int employeeNumber) {
+		this.employeeNumber = employeeNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+}
